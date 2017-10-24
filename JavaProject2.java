cd4197e641802fcbef8ff15dbec961e9f7bae0cc
rajshkr23 <krajshekar12@gail.com>
2017-10-24 17:50:51 -0400

    changes commited

--- JavaProject2.java
+++ JavaProject2.java
@@ -3,7 +3,7 @@ public class JavaProject2{
 	public static void main(String[] args){
 
 		System.out.println("New feature added");
-System.out.println("in Jenkinss");
+System.out.println("in Jenkinss to configure");
 
 	}
 }
