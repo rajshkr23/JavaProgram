09d462c984dc9244458089972a8271cbb91d25d1
rajshkr23 <krajshekar12@gail.com>
2017-10-24 17:54:42 -0400

    msg

--- JavaProject2.java
+++ JavaProject2.java
@@ -2,7 +2,7 @@ public class JavaProject2{
 	
 	public static void main(String[] args){
 
-		System.out.println("New feature added");
+		System.out.println("New feature is added");
 System.out.println("in Jenkinss to configure");
 
 	}
